52ba032bbc49eba627e9254153dbd9038b191234
utterblue <88220098+utterblue@users.noreply.github.com>
2025-02-17 14:37:47 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.utterblue.us.kg": dockerHub,
+  "quay.utterblue.us.kg": "https://quay.io",
+  "gcr.utterblue.us.kg": "https://gcr.io",
+  "k8s-gcr.utterblue.us.kg": "https://k8s.gcr.io",
+  "k8s.utterblue.us.kg" : "https://registry.k8s.io",
+  "ghcr.utterblue.us.kg" : "https://ghcr.io",
+  "cloudsmith.utterblue.us.kg" : "https://docker.cloudsmith.io",
+  "ecr.utterblue.us.kg" : "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker-staging.utterblue.us.kg" : dockerHub,
 };
 
 function routeByHosts(host) {
